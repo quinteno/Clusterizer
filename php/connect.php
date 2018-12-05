73636dfa1453075a885e569c04685191edc70955
Quinten O'Neal <quintenoneal@Quintens-MacBook-Pro.local>
2018-12-04 19:21:11 -0500

    updating database connection file

--- php/connect.php
+++ php/connect.php
@@ -1,5 +1,5 @@
 <?php
 
-$connection = mysqli_connect("localhost" , "root" , "", "1620wdw") or die(mysql_error());
+$connection = mysqli_connect("us-cdbr-iron-east-01" , "b10e6550323004" , "236ab419", "heroku_cbe4789bc831e02") or die(mysql_error());
 
 ?>
